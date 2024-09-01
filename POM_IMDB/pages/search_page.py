1f997b92e88d1a9157f9a3d665f88f069b83b01c
Sivaramakrishnan26 <strsivaram486@gmail.com>
2024-09-01 15:57:20 +0530

    Update search_page.py

--- POM_IMDB/pages/search_page.py
+++ POM_IMDB/pages/search_page.py
@@ -1,37 +1,43 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
-import time
 from selenium.common.exceptions import MoveTargetOutOfBoundsException
 from selenium.common.exceptions import ElementClickInterceptedException
+import time
 
 
 class SearchPage(BasePage):
     EXPAND_ALL = (By.XPATH, "//span[text()='Expand all']")
     NAME = (By.XPATH, "//input[@aria-label='Name']")
+    BIRTH_DATE_FROM = (By.ID, "text-input__10")
+    BIRTH_DATE_TO = (By.ID, "text-input__11")
     SEE_RESULTS = (By.XPATH, "//span[text()='See results']")
 
-    def scroll(self):  # Method to scroll to the element
+    def expand(self):  # Method to scroll to expand and click it
         try:
             self.find_element(self.EXPAND_ALL)
             self.scroll_to_element(self.EXPAND_ALL)
+            self.click_element(self.EXPAND_ALL)
         except MoveTargetOutOfBoundsException as e:
             print(f"Exception Occurred: {e}")
-
-    def click(self):  # Method to click the element
-        try:
-            time.sleep(2)
-            self.click_element(self.EXPAND_ALL)
         except ElementClickInterceptedException as e:
             print(f"Exception Occurred: {e}")
 
-    def enter_name(self, name):  # Method to send keys to the element
+    def enter_text(self, name, birth_date_from, birth_date_to):  # Method to send keys to the element
         self.find_element(self.NAME)
         self.scroll_to_element(self.NAME)
         self.send_keys(self.NAME, name)
+        self.find_element(self.BIRTH_DATE_FROM)
+        self.scroll_to_element(self.BIRTH_DATE_FROM)
+        self.send_keys(self.BIRTH_DATE_FROM, birth_date_from)
+        self.find_element(self.BIRTH_DATE_TO)
+        self.scroll_to_element(self.BIRTH_DATE_TO)
+        self.send_keys(self.BIRTH_DATE_TO, birth_date_to)
 
     def see_results(self):  # Method to See results
         try:
+            time.sleep(2)
+            self.find_element(self.SEE_RESULTS)
+            self.scroll_to_element(self.SEE_RESULTS)
             self.click_element(self.SEE_RESULTS)
-            print("Clicked on SEE_RESULTS button")
         except ElementClickInterceptedException as e:
             print(f"Exception Occurred: {e}")
